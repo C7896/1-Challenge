0517fe54af9b10cf3cdb0300ca21139330e3625e
C7896 <91509809+C7896@users.noreply.github.com>
2024-01-04 15:49:28 -1000

    Add files via upload

---
+++ Intro1Screen.js
@@ -0,0 +1,50 @@
+import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
+import ClearButton from "../components/clearButton";
+import LargeImage from "../components/largeImage";
+const joy = require("../assets/Joy.png");
+
+export default function Intro1Screen({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <LargeImage src={joy} />
+            <View style={styles.hContainer} >
+                <View style={[styles.container, styles.textContainer]}>
+                    <Text style={[styles.text, styles.title]}>Our Mission</Text>
+                    <Text style={[styles.text, styles.body]}>Improve the world</Text>
+                    <Text style={[styles.text, styles.body]}>by 1% everyday.</Text>
+                    <ClearButton title="Next" nav={navigation} destination="Intro2" />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFCF5B",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
+    textContainer: {
+        flexGrow: 6,
+        alignItems: "left",
+        paddingLeft: "10%",
+    },
+    hContainer: {
+        flexDirection: "row",
+    },
+    text: {
+        color: "white",
+        paddingLeft: 3,
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: "bold",
+    },
+    body: {
+        fontSize: 30,
+        fontWeight: "normal",
+    },
+});
---
+++ Intro2Screen.js
@@ -0,0 +1,50 @@
+import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
+import ClearButton from "../components/clearButton";
+import LargeImage from "../components/largeImage";
+const relationship = require("../assets/Relationship.png");
+
+export default function Intro2Screen({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <LargeImage src={relationship} />
+            <View style={styles.hContainer} >
+                <View style={[styles.container, styles.textContainer]}>
+                    <Text style={[styles.text, styles.title]}>How</Text>
+                    <Text style={[styles.text, styles.body]}>Daily challenges that</Text>
+                    <Text style={[styles.text, styles.body]}>improve you and</Text>
+                    <Text style={[styles.text, styles.body]}>those around you.</Text>
+                    <ClearButton title="Next" nav={navigation} destination="Intro3" />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#E4A7C1",
+        alignItems: "left",
+        justifyContent: "center",
+    },
+    textContainer: {
+        flex: 1,
+        paddingLeft: "10%",
+    },
+    hContainer: {
+        flexDirection: "row",
+    },
+    text: {
+        color: "white",
+        paddingLeft: 3,
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: "bold",
+    },
+    body: {
+        fontSize: 30,
+        fontWeight: "normal",
+    },
+});
