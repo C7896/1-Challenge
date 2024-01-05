93c127cab9dd8ca9ca0d2f98c255332a4cf60a9f
rustinkatsura <155695355+rustinkatsura@users.noreply.github.com>
2024-01-04 15:31:07 -1000

    Add files via upload

---
+++ screens/Challenge1Screen.js
@@ -0,0 +1,103 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, ImageBackground, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const backgroundChallengeOne = require('./assets/backgroundChallengeOne.png');
+
+const HomeScreen = () => {
+    return (
+      <SafeAreaView style={styles.container}>
+      <Journal month="Oct" day="20" challenge="Say thanks!"/>
+      <View style={{flex: 1}}/>
+      <ImageBackground source={backgroundChallengeOne} style={styles.backgroundimage}>
+        <Text style={styles.timer}>
+          10:29 left
+        </Text>
+        <Text>
+
+        </Text>
+        <Text style={styles.titleText}>
+          New Challenge
+        </Text>
+        <Text>
+
+        </Text>
+      </ImageBackground>
+
+      <View style={{flex: 0.6}}/>
+
+      <View style={styles.bottomContainer}>
+        <View style={styles.streak}>
+          <Text style={styles.streakText}>10 🔥</Text>
+        </View>
+        <View style={{flex: 0.2}}/>
+        <View style={styles.button}>
+          <Pressable style={{alignItems: 'center'}} onPress={() => console.log('Pressed')}>
+            <Text style={styles.unlockText}>
+              Unlock
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+      
+    </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF815E',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  backgroundimage: {
+    justifyContent: 'center',
+    width: '100%',
+    height: 402.94,
+  },
+  titleText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: "#FFF",
+    textAlign: 'center',
+  },
+  timer: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: "#FFF",
+    textAlign: 'center',
+  },
+  button: {
+    borderWidth: 2,
+    borderColor: "#FFF",
+    borderRadius: 15,
+    width: 182,
+    height: 60, 
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  streak: {
+    backgroundColor: '#FFC0A2',
+    width: 82,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+  streakText: {
+    fontSize: 20,
+    fontWeight: 'Bold',
+    color: "#FFF",
+  },
+  unlockText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff', 
+  },
+
+});
