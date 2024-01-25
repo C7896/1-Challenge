63e9722cd56ae30b6bd1b9215b5aa9c44eae4b78
C7896 <91509809+C7896@users.noreply.github.com>
2024-01-25 17:21:15 -0500

    Delay splash added

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,8 +1,19 @@
 import { View, Text, StyleSheet, SafeAreaView, Pressable, useWindowDimensions } from "react-native";
+import React, { useEffect } from "react";
 
 export default function SplashScreen({ navigation }) {
 
     const windowWidth = useWindowDimensions().width
+
+    useEffect(() => {
+        const delayNavigation = setTimeout(() => {
+          // Navigate to the desired screen after 1 second
+          navigation.navigate("Home Tabs"); // Replace 'YourScreen' with the actual screen name
+        }, 1000);
+    
+        // Cleanup the timeout when the component unmounts
+        return () => clearTimeout(delayNavigation);
+      }, [navigation]);
 
     return (
         <SafeAreaView style={styles.container}>
